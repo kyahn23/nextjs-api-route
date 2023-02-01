ec8c3656310ca7614c8d539448118c0f4350173c
kyahn23 <kyahn23@gmail.com>
2023-02-02 01:52:29 +0900

    mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -6,6 +8,28 @@ function handler(req, res) {
       res.status(422).json({ message: "Invalid email address!" });
       return;
     }
+
+    const url =
+      "mongodb+srv://kyahn:3j3IwKSAIYw6nj50@cluster0.7jehjja.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(url);
+
+    try {
+      const db = client.db("newsletter");
+      const emails = db.collection("emails");
+
+      await emails.insertOne({ email: userEmail });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("mongodb client close");
+      await client.close();
+    }
+    // MongoClient.connect(
+    //   "mongodb+srv://kyahn:3j3IwKSAIYw6nj50@cluster0.7jehjja.mongodb.net/newsletter?retryWrites=true&w=majority"
+    // ).then((client) => {
+    //   const db = client.db();
+    //   db.collection("emails").insertOne(userEmail);
+    // });
 
     console.log(userEmail);
     res.status(201).json({ message: "Signed up!" });
